e243e591e2af5281df5a0d1b3056edc0ee88d9bc
Arseniy <89991971+ImCluzzy@users.noreply.github.com>
2024-07-28 00:05:35 +0300

    new version

---
+++ jobs/telgram.py
@@ -0,0 +1,38 @@
+import main
+import telebot
+
+def send_message_with_attachments_to_telegram(text, attachments):
+    print("Отправка сообщения и медиа в Telegram...")
+    media = []
+    photo_attachments = []
+    for attachment in attachments:
+        if attachment['type'] == 'photo':
+            photo_attachments.append(attachment['photo'])
+
+    if photo_attachments:
+        photo_url = get_largest_photo_url(photo_attachments[0])
+        caption = truncate_text(text)  # Truncate caption to fit within 1024 characters
+        photo = telebot.types.InputMediaPhoto(media=photo_url, caption=caption, parse_mode='HTML')
+        media.append(photo)
+
+        for photo_attachment in photo_attachments[1:]:
+            photo_url = get_largest_photo_url(photo_attachment)
+            photo = telebot.types.InputMediaPhoto(media=photo_url)
+            media.append(photo)
+
+    main.bot.send_media_group(main.tg_channel, media)
+
+def send_message(text):
+    print("Отправка сообщения в Telegram...")
+    truncated_text = truncate_text(text)
+    main.bot.send_message(main.tg_channel, truncated_text)
+
+def get_largest_photo_url(photo):
+    sizes = ['w', 'z', 'y', 'x', 'm']
+    for size in sizes:
+        if size in photo:
+            return photo[size]
+    return photo['sizes'][-1]['url']
+
+def truncate_text(text, limit=1024):
+    return text[:limit] + "..." if len(text) > limit else text
--- main.py
+++ main.py
@@ -2,77 +2,51 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import telebot
 import threading
+import yaml
+import os
+import jobs
 
-bot = telebot.TeleBot("tg_api")
-vk = vk_api.VkApi(token='vk_api')
-long = VkBotLongPoll(vk, 'id_group_vk')
-channel = "@id_group_tg"
+# Load configuration from config.yml
+with open("config.yml", "r") as config_file:
+    config = yaml.safe_load(config_file)
 
+# Extract configuration data
+tg_token = config["tg"]["token"]
+tg_channel = "@" + config["tg"]["channel"]
+vk_token = config["vk"]["token"]
+vk_group_id = config["vk"]["long"]
+
+# Initialize Telegram and VK bots with loaded configuration
+bot = telebot.TeleBot(tg_token)
+vk = vk_api.VkApi(token=vk_token)
+vk.http.headers.update({'Connection': 'close'})  # Обновляем заголовки запроса для закрытия соединения после каждого запроса
+vk.http.timeout = 1000  # Устанавливаем тайм-аут на 10 секунд (или любое другое подходящее значение)
+long = VkBotLongPoll(vk, vk_group_id)
 
 def vk_bot():
     print('Подключение к VK, успешно!')
-
     for event in long.listen():
         if event.type == VkBotEventType.WALL_POST_NEW:
             print('Новый пост!')
-
             if event.obj.attachments:
                 attachments = event.obj.attachments
                 text = event.obj.text if event.obj.text else ""
-                send_message_with_attachments_to_telegram(text, attachments)
-
+                jobs.telgram.send_message_with_attachments_to_telegram(text, attachments)
             elif event.obj.text:
                 text = event.obj.text
-                send_message(text)
-
+                jobs.send_message(text)
         else:
             print(event.type)
         print()
 
-
-def send_message(text):
-    print("Отправка сообщения в Telegram...")
-
-    bot.send_message(channel, text)
-
-
-def send_message_with_attachments_to_telegram(text, attachments):
-    print("Отправка сообщения и медиа в Telegram...")
-    media = []
-
-    for attachment in attachments:
-        photo_url = get_largest_photo_url(attachment['photo'])
-        photo = telebot.types.InputMediaPhoto(photo_url)
-        media.append(photo)
-    print('Я тут!')
-    for m in media:
-        bot.send_photo(channel, m.media, caption=text, parse_mode='HTML')
-
-
-def get_largest_photo_url(photo):
-    sizes = ['w', 'z', 'y', 'x', 'm']
-    for size in sizes:
-        if size in photo:
-            return photo[size]
-    return photo['sizes'][-1]['url']
-
-
-def get_video_url(video):
-    video_url = video['player']
-    return video_url
-
-
 def main():
     vk_thread = threading.Thread(target=vk_bot)
     vk_thread.start()
-
     tg_thread = threading.Thread(target=bot.polling)
     tg_thread.start()
 
-    # Ожидание завершения всех активных потоков перед выходом
     while threading.active_count() > 1:
         pass
-
 
 if __name__ == '__main__':
     main()
